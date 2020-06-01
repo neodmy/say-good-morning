c61a5cc9efd13fc7d4b0d166a1f3c33faee55e3c
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-06-01 08:49:02 +0200

    refactor: declarative way and add value selection (#1)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
+/* eslint-disable max-len */
 const translate = require('@vitalets/google-translate-api');
 const { langs } = require('./langs');
 const slackEmojis = require('./slack-emojis.json');
 
-const getRandom = (item, type) => {
+const generateRandomNumber = (limit) => Math.floor(Math.random() * (limit - 0));
+
+const valueOrRandom = (value, item) => (value && !!(value < item.length) && value) || generateRandomNumber(item.length);
+
+const getRandom = (item, type, value) => {
   const types = {
-    array: () => item[Math.floor(Math.random() * (langs.length - 0))],
-    object: () => Object.keys(item)[Math.floor(Math.random() * (langs.length - 0))],
+    array: () => item[valueOrRandom(value, item)],
+    object: () => {
+      const objectArray = Object.keys(item);
+      return objectArray[valueOrRandom(value, objectArray)];
+    },
   };
   return types[type]();
 };
 
+const hasEmoji = (name) => !!slackEmojis[name];
+
+const flagEmojiOrRandom = (flagEmojiName) => (hasEmoji(flagEmojiName) && flagEmojiName) || getRandom(slackEmojis, 'object');
+
 const generateGoodMorning = () => {
   const randomLanguage = getRandom(langs, 'array');
-  const flag = slackEmojis[`flag-${randomLanguage.code}`];
-  const emoji = (flag && Object.keys(slackEmojis).find((key) => slackEmojis[key] === flag)) || getRandom(slackEmojis, 'object');
+  const flagEmojiName = `flag-${randomLanguage.code}`;
+  const selectedEmoji = flagEmojiOrRandom(flagEmojiName);
   translate('Good Morning', { to: randomLanguage.code }).then((res) => {
-    process.stdout.write(`*${res.text}* (${randomLanguage.lang}) :${emoji}:\n`);
+    process.stdout.write(`*${res.text}* (${randomLanguage.lang}) :${selectedEmoji}:\n`);
   }).catch(() => {});
 };
 
